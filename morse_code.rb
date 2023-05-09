50a8f4d982eaacdfc9e23577d48214e871ba4127
David Espino <davidespinog@gmail.com>
2023-05-09 10:38:57 -0600

    Add decode_string method

--- morse_code.rb
+++ morse_code.rb
@@ -1,40 +1,28 @@
-  # Declare a morse Hash
-  MORSE_ALPHABET = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
+def decode_character(character)
+  morse_alphabet = {
+    '.-' => 'A', '-...' => 'B',
+    '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V',
+    '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
   }
+  morse_alphabet[character]
+end
 
-  def decode_character(character)
-    MORSE_ALPHABET[character]
-  end
+def decode_word(word)
+  word.split.map { |character| decode_character(character) }.join
+end
 
-  def decode_word(word)
-    word.split(' ').map { |character| decode_character(character) }.join('')
-  end
+def decode_string(string)
+  string.split('  ').map { |word| decode_word(word) }.join(' ')
+end
 
-  puts decode_character('-.')
-  puts decode_word('.... . .-.. .-.. ---');
+puts decode_string('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
