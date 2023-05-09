754fc4e03e2aa82b9aa7665a6da096ffe9862996
Jaime Villegas <jaimevillegas296@gmail.com>
2023-05-09 11:23:20 -0500

    Add decode_character and decode_word methods

---
+++ morse_code.rb
@@ -0,0 +1,40 @@
+  # Declare a morse Hash
+  MORSE_ALPHABET = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+
+  def decode_character(character)
+    MORSE_ALPHABET[character]
+  end
+
+  def decode_word(word)
+    word.split(' ').map { |character| decode_character(character) }.join('')
+  end
+
+  puts decode_character('-.')
+  puts decode_word('.... . .-.. .-.. ---');
